d9588714f39f5190528e3d5bd052af4591f4c31a
jfh01 <joeshmoe@world.std.com>
2017-01-08 18:06:51 -0500

    improved config documentation

--- src/diy-lpr.py
+++ src/diy-lpr.py
@@ -4,32 +4,64 @@ import traceback
 import threading
 import random
 import sys
+import os
 
 import camcap
 import recognizer
 
 import time
 
-PROJECT_DIR = "/home/jascha/projects/diy-lpr/work"
+## BEGIN DIY-ALPR CONFIG
 
-CAPTURE_DIR = PROJECT_DIR + "/capture"
-CAPTURE_CTL_FILE = PROJECT_DIR + "/capture_ctl" # if specified, only capture when this file is present
+# PROJECT_DIR - the path to the working directory where images and output will be stored
+PROJECT_DIR = os.path.dirname(os.path.realpath(__file__)) + "/../work" # assume it is "../work"
+# PROJECT_DIR = "/home/USER/diy-alpr/src/work" # you could hard code this
 
+# CAPTURE_DIR - where to store captured images
+CAPTURE_DIR = PROJECT_DIR + "/capture" 
+
+# CAPTURE_CTL_FILE (optional) - if specified, images will only be captured when this file 
+#                               is present -- allows you to pause/resume  capture
+CAPTURE_CTL_FILE = PROJECT_DIR + "/capture_ctl"
+
+# MAX_FILES - max number of files to have in CAPTURE_DIR. capturing will pause until the number of files drops
+#             this helps prevent you from running out of disk space if you are capturing images faster than you
+#             can recognize them
 MAX_FILES = 10000
 
-POSTPROC_HIT_DIR = PROJECT_DIR  + "/proc-hit" # where to put files w/ hit after processing
-POSTPROC_LOWCONF_DIR = PROJECT_DIR  + "/proc-lowconf" # where to put files w/ low confidence after processing
-#POSTPROC_NOHIT_DIR = PROJECT_DIR  + "/work/proc-nohit" # where to put files w/ no hit after processing
-POSTPROC_NOHIT_DIR = None # where to put files w/ no hit after processing - None deletes the files
+# POSTPROC_HIT_DIR - where to put files with a license plate hit after recognition
+POSTPROC_HIT_DIR = PROJECT_DIR  + "/proc-hit"
 
+# POSTPROC_HIT_LOWCONF_DIR (optional) - where to put files with a low confidence hit after recognition. If None,
+#                                       these files will be deleted
+POSTPROC_LOWCONF_DIR = PROJECT_DIR  + "/proc-lowconf"
+
+# POSTPROC_NOHIT_DIR (optional) - where to put files with no hit after recognition. If None, these files will be deleted.
+
+POSTPROC_NOHIT_DIR = None
+#POSTPROC_NOHIT_DIR = PROJECT_DIR  + "/work/proc-nohit"
+
+# OUTPUT_JSON (optional, if OUTPUT_CSV specified) - on recognition, put a JSON file in this directory with the details of the hit
 OUTPUT_JSON = PROJECT_DIR  + "/output/json"
+# OUTPUT_CSV (optional, if OUTPUT_JSON specified) - on recnognition, add a line to the CSV file with details of the HIT
 OUTPUT_CSV = PROJECT_DIR  + "/output/output.csv"
 
+# DEFAULT_REGION (optional) - The default OpenALPR region (or US state) to try to pattern match license plate strings.
+#                             See Open ALPR documentation for more detail
 DEFAULT_REGION = "ma"
+
+# MIN_CONF_PATTERNMATCH - Minimum OpenALPR confidence to consider a plate a "hit" when the plate string pattern matches
+#                         the pattern for the DEFAULT_REGION
 MIN_CONF_PATTERNMATCH = 75.0
+
+# MIN_CONF_PATTERNMATCH - Minimum OpenALPR confidence to consider a plate a "hit" when the plate string pattern does not 
+#                         match the pattern for the DEFAULT_REGION
 MIN_CONF_NOPATTERNMATCH = 85.0
 
-RECOGNIZER_THREADS = 3 # we have four processor cores
+# RECOGNIZER_THREADS - How many threads to launch to perform recognition. Recommend # of processor cores minus one.
+RECOGNIZER_THREADS = 3 # we have four processor cores on the Raspberry Pi 3 Model B
+
+## END DIY-ALPR CONFIG
 
 try:
     print "diy-lpr - setting up camcap"
